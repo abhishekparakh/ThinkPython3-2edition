7767ec2c639e1009ac39169d21cbe93985298293
PKI-IST\aparakh <abhishek.parakh@gmail.com>
2016-08-12 16:30:39 -0500

    Intial Commit

---
+++ Practice Code.py
@@ -0,0 +1,17 @@
+'''
+Scratch pad for practice code while reading the book
+"Think Python 3 - Second Edition"
+'''
+
+
+import turtle
+bob = turtle.Turtle()
+print(bob)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+turtle.mainloop()
