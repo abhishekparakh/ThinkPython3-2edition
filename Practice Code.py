14918b069e232e3389dbe372f3b9733cc55ce73b
PKI-IST\aparakh <abhishek.parakh@gmail.com>
2016-08-13 23:38:16 -0500

    Exercise 6.5, Euclid GCD

--- Practice Code.py
+++ Practice Code.py
@@ -6,11 +6,10 @@ Scratch pad for practice code while reading the book
 import turtle
 
 
-def is_power(a,b):
-    if a/b == 1:
-        return True
-    if a%b != 0:
-        return False
-    return is_power(a/b,b)
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
 
-print(is_power(27,3))
+print(gcd(27,15))
